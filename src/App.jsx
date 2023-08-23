04831cbd8db24308b169b602d6d0d4c351166afa
Aryan Khatik <rkhatik156@gmail.com>
2023-08-23 14:25:43 +0530

    second commit of the day

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import Test from "./Components/Test";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Products from "./Components/Products";
+import Contacts from "./Components/Contacts";
+import SingleProduct from "./Components/SingleProduct";
+import Cart from "./Components/Cart";
+import Error from "./Components/Error";
 function App() {
   return (
     <>
-      <Test />J
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
     </>
   );
 }
