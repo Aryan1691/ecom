52b71c1233a022bded09f3e1624ac83393700000
Aryan Khatik <rkhatik156@gmail.com>
2023-08-31 15:47:23 +0530

    added Service.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,15 @@
 import HeroSection from "./HeroSection";
+import Service from "./Service";
+import Trusted from "./Trusted";
 const Home = () => {
-  
   return (
+    <>
       <HeroSection name={"Aryan Store"} />
-   
+      <Service />
+      <Trusted />
+    </>
   );
 };
+
 
 export default Home;
